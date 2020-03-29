ac706b672b29011684596f0d69f099aa47a3c5b4
Shanay Murdock <murdock.shanay@gmail.com>
2020-03-29 19:33:56 -0400

    Push budget to on-screen summary

--- scripts/app.js
+++ scripts/app.js
@@ -99,7 +99,11 @@ const UIController = (() => {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   return {
@@ -149,6 +153,22 @@ const UIController = (() => {
       fieldsArr[0].focus();
     },
 
+    displayBudget: obj => {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent =
+        obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          obj.percentage + '%';
+      } else {
+        document.querySelector(
+          DOMstrings.percentageLabel
+        ).textContent = obj.percentage = '---';
+      }
+    },
+
     getDOMstrings: () => {
       return DOMstrings;
     }
@@ -177,7 +197,7 @@ const controller = ((budgetCtrl, UICtrl) => {
     let budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   const ctrlAddItem = () => {
@@ -207,6 +227,12 @@ const controller = ((budgetCtrl, UICtrl) => {
   return {
     init: () => {
       console.log('Application has started.');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
